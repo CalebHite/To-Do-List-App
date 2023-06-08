07b34d210d208f54818d7f376f60907fea0313f5
CalebHite <96922867+CalebHite@users.noreply.github.com>
2023-06-08 10:27:13 -0500

    Add 'Add Item' Functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   export const ssr = false;
   import { auth, db as firestore } from './firebase';
   import { collectionStore, userStore } from 'sveltefire';
-  import { getDatabase, ref, set } from "firebase/database";
+  import { collection, addDoc } from "firebase/firestore";
   import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { form, field } from 'svelte-forms';
+  import { required } from 'svelte-forms/validators';
 
   const provider = new GoogleAuthProvider();
 
@@ -25,25 +27,44 @@
 
   const items = collectionStore(firestore, 'items');
 
-  // function newItem(id: number, name: string, desc: string, time: number, completeBy: number) {
-  //   set(ref(firestore, 'items/' + id), {
-  //     name: name,
-  //     description: desc,
-  //     completeBy: completeBy,
-  //     time: time
-  //     });
-  // }
+  function newItem(id: number, name: string, desc: string, time: number, completeBy: string) {
+    addDoc(collection(firestore, "items"), {
+      name: name,
+      desc: desc,
+      time: time,
+      completeBy: completeBy
+    });
+  }
+  
+  let name = "";
+  let time = 0;
+  let whenComplete = "";
+  let desc = "";
+
 </script>
 
 {#if $user}
     <p>Hi {$user.displayName}</p>
 
+    <section>
+      <label for="itemName">Name:</label>
+      <input type="text" id="itemName" bind:value={name} />
+      <label for="itemDesc">Description:</label>
+      <input type="text" id="itemDesc" bind:value={desc} />
+      <label for="itemTime">Hours to Complete:</label>
+      <input type="number" id="itemTime" bind:value={time} />
+      <label for="itemComp">Complete By:</label>
+      <input type="text" id="itemComp" bind:value={whenComplete} />
+
+      <button on:click={()=>{newItem($items.length - 1, name, desc, time, whenComplete)}}>Add Item</button>
+    </section>
+
     {#each $items as i}
     <div>
       <h3>{i.name}</h3>
-      <h4>{i.description}</h4>
+      <h4>{i.desc}</h4>
       <h4>{i.time} hours to complete</h4>
-      <h4>Finish by {i.whenComplete}</h4>
+      <h4>Finish by {i.completeBy}</h4>
     </div>
     {/each} 
 {:else}
